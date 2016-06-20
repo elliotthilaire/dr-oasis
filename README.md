fab92760d324606c6c94ea1f4d23edf478f7c2ef
Elliott Hilaire <elliott@kallisti.lan>
2016-06-20 13:28:53 +0100

    Update with message in README

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 DR. OASIS
 =========
 
+I don't use this anymore. I have adopted the use of Homebrew/homebrew-bundle which the Brewfile store in my dotfiles repo.
+
 ## Disaster Recovery: OSX Automatic Software Installation Script
 
 Install my favourite software packages with one command using Homebrew and Cask.
--- dr-oasis.sh
+++ dr-oasis.sh
@@ -16,7 +16,11 @@
 
 # Install [Homebrew](http://homebrew.io/)
 
-    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Update homebrew
+
+    brew update
 
 # Install some commonly required packages
 
@@ -24,12 +28,7 @@
 
 # Install [Cask](http://caskroom.io/)
 
-    brew install caskroom/cask/brew-cask
-
-# Install to /Applications
-# I want to make my software available to other user accounts on the same machine.
-
-    export HOMEBREW_CASK_OPTS="--appdir=/Applications"
+    brew tap caskroom/cask
 
 # Install brews
 # ( the sed command strips out comments )
@@ -38,7 +37,11 @@
 
 # Install casks
 
-    brew cask install $(cat Caskfile | sed 's/#.*//')
+#    brew cask install $(cat Caskfile | sed 's/#.*//')
+
+# Cleanup
+
+    brew cleanup
 
 # Resources
 #. https://mattstauffer.co/blog/setting-up-a-new-os-x-development-machine-part-2-global-package-managers
